06e1fd99a81a0e667c0e960131aaf2bf23b408f2
totopizza <gren6158@yahoo.co.jp>
2016-01-21 11:16:30 +0900

    Backup - Miku_countup

--- Miku_countup/main.cpp
+++ Miku_countup/main.cpp
@@ -17,6 +17,8 @@ struct Miku
 	bool is_show;
 };
 
+Miku mikus[MAX];
+
 bool Collision(Vec2f _mouse_position, Vec2f _miku_position, Vec2f _miku_size){
 	if (_mouse_position.x() > _miku_position.x() &&
 		_mouse_position.x() < _miku_position.x() + _miku_size.x() &&
@@ -30,18 +32,17 @@ bool Collision(Vec2f _mouse_position, Vec2f _miku_position, Vec2f _miku_size){
 int main() {
 	AppEnv env(Window::WIDTH, Window::HEIGHT);
 
-	Miku miku[MAX];
 	Random random;
 	random.setSeed(u_int(time(nullptr)));
 
 	Texture image = Texture("res/miku.png");
 
 	for (int i = 0; i < MAX; ++i){
-		miku[i].position = Vec2f(random(-WIDTH / 2, WIDTH / 2 - 200), random(-HEIGHT / 2, HEIGHT / 2 - 200));
-		miku[i].size = Vec2f(320, 270);
-		miku[i].clip_position = Vec2f(0, 0);
-		miku[i].clip_size = Vec2f(320, 270);
-		miku[i].is_show = false;
+		mikus[i].position = Vec2f(random(-WIDTH / 2, WIDTH / 2 - 200), random(-HEIGHT / 2, HEIGHT / 2 - 200));
+		mikus[i].size = Vec2f(320, 270);
+		mikus[i].clip_position = Vec2f(0, 0);
+		mikus[i].clip_size = Vec2f(320, 270);
+		mikus[i].is_show = false;
 	}
 
 	int count = 600;
@@ -50,27 +51,74 @@ int main() {
 		// 開始
 		env.begin();
 
-		Vec2f mouse_posision = env.mousePosition();
-
 		count -= 1;
-		for (int i = 0; i < MAX; i++){
-			if (miku[i].is_show == false && count == 0){
-				miku[i].is_show = true;
-				count = 600;
+
+		if (count == 0){
+			for (int i = 0; i < MAX; ++i){
+				if (mikus[i].is_show) continue;
+				mikus[i].is_show = true;
 				break;
 			}
 		}
 
+		if (env.isPushButton(LEFT)){
 
+			Vec2f mouse_position = env.mousePosition();
+
+			for (int i = 0; i < MAX; ++i){
+
+				if (!mikus[i].is_show) continue;
+
+				if (Collision(mouse_position, mikus[i].position, mikus[i].size)){
+					mikus[i].is_show = false;
+					break;
+				}
+			}
+		}
+
+		for (int i = 0; i < MAX; ++i){
+			if (!mikus[i].is_show) continue;
+				drawTextureBox(mikus[i].position.x(), mikus[i].position.y(),
+					mikus[i].size.x(), mikus[i].size.y(),
+					mikus[i].clip_position.x(), mikus[i].clip_position.y(),
+					mikus[i].clip_size.x(), mikus[i].clip_size.y(),
+					image);
+		}
+
+		/*count -= 1;
+		if (count == 0){
+			for (int i = 0; i < MAX; i++){
+				if (mikus[i].is_show) continue;
+
+				mikus[i].is_show = true;
+				break;
+			}
+		}
+
+		if (env.isPushButton(LEFT)){
+
+			Vec2f mouse_position = env.mousePosition();
+
+			for (int i = 0; i < MAX; i++){
+
+				if (!mikus[i].is_show) continue;
+
+				if (Collision(mouse_position, mikus[i].position, mikus[i].size)){
+					mikus[i].is_show = false;
+					break;
+				}
+			}
+		}
 
 		for (int i = 0; i < MAX; i++){
-			if (miku[i].is_show){
-			drawTextureBox(miku[i].position.x(), miku[i].position.y(),
-						   miku[i].size.x(), miku[i].size.y(),
-						   miku[i].clip_position.x(), miku[i].clip_position.y(),
-						   miku[i].clip_size.x(), miku[i].clip_size.y(), image);
-			}
-	}
+			if (!mikus[i].is_show) continue;
+
+			drawTextureBox(mikus[i].position.x(), mikus[i].position.y(),
+						   mikus[i].size.x(), mikus[i].size.y(),
+						   mikus[i].clip_position.x(), mikus[i].clip_position.y(),
+						   mikus[i].clip_size.x(), mikus[i].clip_size.y(),
+						   image);
+		}*/
     
     
     // 終了
